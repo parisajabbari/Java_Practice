3936091a2db260f5c6074ff16dc15eec3065aa80
Parisa Jabbari <parisa.jabbari@gmail.com>
2020-07-14 15:51:16 +1000

    Moving away from statics
    removed static methods and fields.
    applied encapsulation.
    created constructor.

--- Mortgage Calculator/com/codewithmosh/MortgageReport.java
+++ Mortgage Calculator/com/codewithmosh/MortgageReport.java
@@ -3,22 +3,31 @@ package com.codewithmosh;
 import java.text.NumberFormat;
 
 public class MortgageReport {
-    public static void printMortgage(int principal, float annualInterest, byte years) {
-        double mortgage = MortgageCalculator.calculateMortgage(principal, annualInterest, years);
+
+
+    private MortgageCalculator calculator;
+
+    public MortgageReport(MortgageCalculator calculator) {
+        this.calculator = calculator;
+    }
+
+    public void printPaymentSchedule() {
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
+        for (short month = 1; month <= calculator.getYears() * Main.MONTHS_IN_YEAR; month++) {
+            double balance = calculator.calculateBalance(month);
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
+    }
+
+    public void printMortgage() {
+
+        double mortgage = calculator.calculateMortgage();
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println();
         System.out.println("MORTGAGE");
         System.out.println("--------");
         System.out.println("Monthly Payments: " + mortgageFormatted);
-    }
-
-    public static void printPaymentSchedule(int principal, float annualInterest, byte years) {
-        System.out.println();
-        System.out.println("PAYMENT SCHEDULE");
-        System.out.println("----------------");
-        for (short month = 1; month <= years * Main.MONTHS_IN_YEAR; month++) {
-            double balance = MortgageCalculator.calculateBalance(principal, annualInterest, years, month);
-            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
-        }
     }
 }
