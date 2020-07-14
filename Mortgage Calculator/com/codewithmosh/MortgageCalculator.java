9d453ab5b9cb2023e3f3cf46ffc10ff75ccee22d
Parisa Jabbari <parisa.jabbari@gmail.com>
2020-07-14 16:56:54 +1000

    Extracting getRemainingBalances()
    changed report class to use calculation from calculator class so it wont contain calculation logic
    applied abstraction on calculator class by removing unused method and made fields private as needed.

--- Mortgage Calculator/com/codewithmosh/MortgageCalculator.java
+++ Mortgage Calculator/com/codewithmosh/MortgageCalculator.java
@@ -2,8 +2,8 @@ package com.codewithmosh;
 
 public class MortgageCalculator {
 
-    public final static byte MONTHS_IN_YEAR = 12;
-    public final static byte PERCENT = 100;
+    private final static byte MONTHS_IN_YEAR = 12;
+    private final static byte PERCENT = 100;
 
     private int principal;
     private float annualInterest;
@@ -38,16 +38,20 @@ public class MortgageCalculator {
         return mortgage;
     }
 
+    public double[] getRemainingBalances(){
+        var balances = new double[getNumberOfPayments()];
+        for (short month = 1; month <= balances.length; month++)
+            balances[month -1] = calculateBalance(month);
+
+        return balances;
+    }
+
     private int getNumberOfPayments() {
         return years * MONTHS_IN_YEAR;
     }
 
     private float getMonthlyInterest() {
         return annualInterest / PERCENT / MONTHS_IN_YEAR;
-    }
-
-    public short getYears() {
-        return years;
     }
 
 }
