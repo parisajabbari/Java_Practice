5c4d9b8133629c04575ddc16b6c4d385ba07b037
Parisa Jabbari <parisa.jabbari@gmail.com>
2020-07-14 15:58:46 +1000

    Refactoring main
    Moving static fields to related MortgageCalculator class

--- Mortgage Calculator/com/codewithmosh/MortgageCalculator.java
+++ Mortgage Calculator/com/codewithmosh/MortgageCalculator.java
@@ -2,6 +2,8 @@ package com.codewithmosh;
 
 public class MortgageCalculator {
 
+    public final static byte MONTHS_IN_YEAR = 12;
+    public final static byte PERCENT = 100;
     private int principal;
     private float annualInterest;
     private byte years;
@@ -13,8 +15,8 @@ public class MortgageCalculator {
     }
 
     public double calculateBalance(short numberOfPaymentsMade) {
-        float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
-        float numberOfPayments = years * Main.MONTHS_IN_YEAR;
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        float numberOfPayments = years * MONTHS_IN_YEAR;
 
         double balance = principal
                 * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
@@ -25,8 +27,8 @@ public class MortgageCalculator {
 
     public double calculateMortgage() {
 
-        float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
-        float numberOfPayments = years * Main.MONTHS_IN_YEAR;
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        float numberOfPayments = years * MONTHS_IN_YEAR;
 
         double mortgage = principal
                 * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
