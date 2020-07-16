426c50c903f32a2ab6d5f90bd4156cfc3ebe83b2
Parisa Jabbari <parisa.jabbari@gmail.com>
2020-07-16 14:29:53 +1000

    Add interfaces
    Decouple the process class by implementing interfaces for each service it needs.

--- MyTube/src/com/mytube/VideoProcessor.java
+++ MyTube/src/com/mytube/VideoProcessor.java
@@ -1,15 +1,21 @@
 package com.mytube;
 
 public class VideoProcessor {
+
+    private VideoEncoder videoEncoder;
+    private VideoDatabase database;
+    private NotificationService Notification;
+
+    public VideoProcessor(VideoEncoder videoEncoder, VideoDatabase database, NotificationService Notification) {
+        this.videoEncoder = videoEncoder;
+        this.database = database;
+        this.Notification = Notification;
+    }
+
     public void process(Video video) {
-        var encoder = new VideoEncoder();
-        encoder.encode(video);
-
-        var database = new VideoDatabase();
+        videoEncoder.encode(video);
         database.store(video);
-
-        var emailService = new EmailService();
-        emailService.sendEmail(video.getUser());
+        Notification.notify(video.getUser());
     }
 }
 
